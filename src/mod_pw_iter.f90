899fb4d71a79cf3b698fb46cd55eb51081012250
Philippe Pepiot <philippe.pepiot@gmail.com>
2009-01-16 14:10:48 +0100

    mod_pw_iter.f90: Test sur les matrices

--- src/mod_pw_iter.f90
+++ src/mod_pw_iter.f90
@@ -13,18 +13,27 @@ contains
     real, dimension(size(u,1)) :: x,y
     real :: pw_iter
     real :: eps = 1
-    ! On va jusqu'à l'ordre 10000 ce qui est largement suffisant
 
-    if(size(u,2) /= 1 .or. size(A,1) /= size(A,2)) then
+    pw_iter = 0.
+
+    ! On teste si la matrice est bien carrée et si u est bien un vecteur
+    if(size(u,2) /= 1 .or. size(A,1) /= size(A,2) .or. size(u,1) /= size(A,2)) then
        write(0,*) 'Matrices non conformes'
     else
        x  = u(:,1)
-       do while(eps > 0.0001)
-          y = x
-          x = matmul(A,x)/maxval(abs(matmul(A,x)))
-          eps = maxval(abs(x-y))
-       end do
-       pw_iter = maxval(abs(matmul(A,x)))
+       ! On teste si le vecteur est bien de norme 1
+       if(maxval(abs(x)) /= 1) then
+          write(0,*) 'Erreur, le premier vecteur doit être de norme infinie = 1'
+       else
+          ! Ici commence l'algorithme
+          do while(eps > 0.001)
+             y = x
+             x = matmul(A,x)/maxval(abs(matmul(A,x)))
+             eps = maxval(abs(x-y))
+             print*,'eps =', eps
+          end do
+          pw_iter = maxval(abs(matmul(A,x)))
+       end if
 
     end if
   end function pw_iter
